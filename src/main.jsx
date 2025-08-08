54f00aafedf27bac54e015074a184d88dc0dee6c
zion1905 <brindhazion@gmail.com>
2025-08-08 06:18:10 +0530

    check the style error

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom'; // ✅ Must import this
-import './styles/navbar.css'; // or wherever your global styles are
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
