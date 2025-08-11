ef712897c951ef08e9f7a576221397ee9385094d
zion1905 <brindhazion@gmail.com>
2025-08-11 12:22:15 +0530

    Add the details in a vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',   // allows LAN access too
+    port: 5173,        // ensure it always uses 5173
+    strictPort: true,  // fail if 5173 is busy instead of changing port
+    hmr: {
+      protocol: 'ws',  // or 'wss' if using HTTPS
+      host: 'localhost',
+      port: 5173
+    }
+  }
 })
